1b41bbf0b97157bbc063a1cebc3b1eb4a857aa5f
Gaurav Tewari <54894783+tewarig@users.noreply.github.com>
2019-10-31 20:28:48 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,36 @@
+alert("this is dice game between player1 and player2, player getting higer number on dice wins, let's see who wins");
+
+
+var n= Math.random();
+n = n*6;
+var varible1= Math.floor(n) + 1;
+
+var randomimage= "dice" + varible1 + ".png" ;
+
+var imagessrc= "images/" + randomimage;
+
+
+var img1 = document.querySelectorAll("img")[0];
+img1.setAttribute("src",imagessrc);
+
+
+
+var varible2= Math.floor(Math.random()*6) + 1;
+
+var randomimage1= "dice" + varible2 + ".png" ;
+
+var imagessrc1= "images/" + randomimage1;
+
+
+var img2 = document.querySelectorAll("img")[1];
+img2.setAttribute("src",imagessrc1);
+
+if(varible1>varible2){
+  document.querySelector("h1").innerHTML = "player 1 winsss" ;
+}
+else if(varible2>varible1)
+{
+  document.querySelector("h1").innerHTML = "player 2 winsss";
+}
+else
+(document.querySelector("h1").innerHTML = "its  a draw plesae refresh the page");
